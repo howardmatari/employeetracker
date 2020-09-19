f594f98d010ed261b56d3d3f885150197c8144a3
Matari Howard <howardmatari@gmail.com>
2020-09-19 09:45:13 -0400

    Initial changes

---
+++ connection.js
@@ -0,0 +1,13 @@
+const util = require("util");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your username
+    user: "root",
+    // Your password
+    password: "root",
+    database: "employees"
+});
+
+connection.connect();
---
+++ seed.sql
@@ -0,0 +1,34 @@
+use employees;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+    INSERT INTO employee
+        (first_name, Last_name, role_id,manager_id)
+    VALUES
+        ('Mike', 'Miller', 1, NULL),
+        ('Jake', 'Chan',2,1),
+        ('Mary', 'Perez',3,NULL),
+        ('Kevin', 'Ashley',4,3),
+        ('Steven', 'Baldwin',5,NULL),
+        ('Melissa', 'Brown',6,5),
+        ('Sara', 'Lourd',7,NULL),
+        ('Tom', 'Allan',8,7),
+        ('Jason','Chin',8,NULL);
